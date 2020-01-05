c59eca5034491648ab88960a626091e7e2a35bab
vivek <vivek@DESKTOP-IV97UTU>
2020-01-05 14:16:58 +0530

    change in controller

--- propertymanagement2/src/main/java/com/example/propertytest/Controller.java
+++ propertymanagement2/src/main/java/com/example/propertytest/Controller.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/getownerdetail")
 public class Controller {
 static {
+	
+	System.out.println("controller_rwhhftj");
 	System.out.println("controller");
 System.out.println("controller");System.out.println("controller");
 
